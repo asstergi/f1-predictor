55b4d75fc86092f75a81a0d2164d52c49a186acb
asstergi <asstergi@users.noreply.github.com>
2017-05-06 17:27:47 +0300

    Update train_model.R

--- F1-sentiment-analysis/sentiment analysis/train_model.R
+++ F1-sentiment-analysis/sentiment analysis/train_model.R
@@ -46,35 +46,7 @@ tweets_test <- tweets_classified[-trainIndex, ]
  
 ##### doc2vec #####
 # define preprocessing function and tokenization function
-removeURL <- function(tweetText) {
-  gsub("http[[:alnum:][:punct:]]+", "", tweetText)
-}
-#This method preprocess the tweets
-twitter.preprocessTweets <- function(tweets) {
-  #get rid of latin words by cnverting to utf-8
-  tweets = iconv(tweets, "LATIN1", "UTF-8",sub="")
-  tweets = iconv(tweets, "LATIN2", "UTF-8",sub="")
-  #Create corpus out of the tweets
-  tweetCorpus <- Corpus(VectorSource(tweets),readerControl=list(language="en"))
-  #Convert to lowercase
-  tweetCorpus <- tm_map(tweetCorpus, content_transformer(tolower))
-  #Remove the URLs from the tweet
-  tweetCorpus <- tm_map(tweetCorpus, removeURL)
-  #Removes all the punctuation
-  tweetCorpus <- tm_map(tweetCorpus, removePunctuation)
-  #Removes all the numbers
-  tweetCorpus <- tm_map(tweetCorpus, removeNumbers)
-  #removes the stop words 
-  tweetCorpus <- tm_map(tweetCorpus, removeWords, c(stopwords("english"),"rt","http","retweet"))
-  #Stem the document using SnowballC
-  tweetCorpus <- tm_map(tweetCorpus, stemDocument)
-  #Convert to plain text document
-  tweetCorpus <- tm_map(tweetCorpus, PlainTextDocument,language="english")
-  
-  text = data.frame(text = sapply(tweetCorpus, paste, collapse = " "), stringsAsFactors = FALSE)[[1]][1]
-  return(text)
-}
-prep_fun <- tolower #twitter.preprocessTweets
+prep_fun <- tolower
 tok_fun <- word_tokenizer
  
 it_train <- itoken(tweets_train$text, 
